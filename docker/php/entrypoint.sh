8264d6a63750664675bcab2c4db34ddae51ae9ae
Gilles-MUESS <gillesmuess.pro@gmail.com>
2025-04-19 22:56:43 +0200

    Mise à jour Dockerfile PHP et script entrypoint pour test final (image de dev)

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 set -e
 
-# Vérification de l'existence du projet
+APP_USER=${APP_USER:-appuser}
+APP_GROUP=${APP_GROUP:-appgroup}
+APP_UID=${APP_UID:-1000}
+APP_GID=${APP_GID:-1000}
+export HOME=/home/$APP_USER
+export SYMFONY_CLI_HOME=$HOME/.symfony5
+
+# S'assurer que les droits sont bons sur /var/www
+
+cd /var/www
+
+# Création du projet Symfony si nécessaire
 if [ ! -f composer.json ]; then
-    echo "Création d'un nouveau projet Symfony ${SYMFONY_VERSION}..."
-    symfony new . --version="${SYMFONY_VERSION}" --${SYMFONY_TEMPLATE:-webapp} --no-git
-    
-    # Correction des permissions
-    chown -R www-data:www-data .
+	echo "Création du projet Symfony dans /tmp/project..."
+	symfony new /tmp/project --version="${SYMFONY_VERSION}" --webapp --no-git
+
+	# Si un .env a été généré, le renommer en .env.local AVANT la copie
+	if [ -f /tmp/project/.env ]; then
+		mv /tmp/project/.env /tmp/project/.env.local
+	fi
+
+	echo "Copie du projet Symfony (en excluant compose.yaml et .env)..."
+	rsync -a --exclude=compose.yaml /tmp/project/ .
+
+	rm -rf /tmp/project
+
+	composer install --prefer-dist --no-progress --no-interaction
 fi
 
 exec docker-php-entrypoint "$@"
